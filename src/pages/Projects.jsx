8d14640bb2a9b57aec544a2dc8d818c03b9ee07a
jeremiah <bankolejeremiahadeoye@gmail.com>
2025-01-29 18:31:54 +0100

    latest project - caresync - added

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,49 +1,56 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import { link } from 'framer-motion/client';
 
 const projects = [
-  {
-    title: 'Tinka',
-    description: 'A website that helps one Reclaim his or her Mental Health Stability - React, Tailwind, Shadcn',
-    image: '/images/tinka.png',
-    link: "https://tinkahealthservices.com/",
-  },
-  {
-    title: 'Care Dental',
-    description: 'A user-friendly platform that connects individuals with affordable dental care solutions.',
-    image: '/images/caredental.png',
-    link: "https://caredentalsavings.com/",
-  },
-  {
-    title: 'BlogSphere',
-    description: 'A blog hub for reading, commenting, and discovering similar books from favorite authors.',
-    image: '/images/elevate.png',
-    link: "https://elevate-lemon.vercel.app",
-  },
-  {
-    title: 'VBookStore',
-    description: 'Online bookstore with categories, favorites, and seamless cart/purchase options.',
-    image: '/images/vbook.png',
-    link: "https://v-book-one.vercel.app",
-  },
-  {
-    title: 'Tratour',
-    description: 'Tour booking site with details, search, and favorites for a personalized experience.',
-    image: '/images/tratour.png',
-    link: "https://tratour.vercel.app",
-  },
-  {
-    title: 'Eduventure',
-    description: 'A student marketplace for showcasing skills, hiring, and buying services or products.',
-    image: '/images/eduventure.png',
-    link: "https://eduventure-omega.vercel.app",
-  },
-  {
-    title: 'World Atlas',
-    description: 'This is a React-based web application that provides detailed information about countries around the world.',
-    image: '/images/world_atlas.png',
-    link: "https://world-atlas-ten.vercel.app/",
-  }
+    {
+        title: "CareSync - Doctor's Appointments Made Easy",
+        description: 'A platform that helps patients book appointments with doctors and specialists.',
+        image: '/images/caresync.png',
+        link: "https://caresync-frontend.onrender.com/",
+    },
+    {
+        title: 'Tinka',
+        description: 'A website that helps one Reclaim his or her Mental Health Stability - React, Tailwind, Shadcn',
+        image: '/images/tinka.png',
+        link: "https://tinkahealthservices.com/",
+    },
+    {
+        title: 'Care Dental',
+        description: 'A user-friendly platform that connects individuals with affordable dental care solutions.',
+        image: '/images/caredental.png',
+        link: "https://caredentalsavings.com/",
+    },
+    {
+        title: 'BlogSphere',
+        description: 'A blog hub for reading, commenting, and discovering similar books from favorite authors.',
+        image: '/images/elevate.png',
+        link: "https://elevate-lemon.vercel.app",
+    },
+    {
+        title: 'VBookStore',
+        description: 'Online bookstore with categories, favorites, and seamless cart/purchase options.',
+        image: '/images/vbook.png',
+        link: "https://v-book-one.vercel.app",
+    },
+    {
+        title: 'Tratour',
+        description: 'Tour booking site with details, search, and favorites for a personalized experience.',
+        image: '/images/tratour.png',
+        link: "https://tratour.vercel.app",
+    },
+    {
+        title: 'Eduventure',
+        description: 'A student marketplace for showcasing skills, hiring, and buying services or products.',
+        image: '/images/eduventure.png',
+        link: "https://eduventure-omega.vercel.app",
+    },
+    {
+        title: 'World Atlas',
+        description: 'This is a React-based web application that provides detailed information about countries around the world.',
+        image: '/images/world_atlas.png',
+        link: "https://world-atlas-ten.vercel.app/",
+    }
 ];
 
 function Projects() {
